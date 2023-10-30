01955a4498c89734709b5516dbf527bb98235b7d
Greg Rell <rell.greg@gmail.com>
2023-10-29 21:18:46 -0400

    import of react-icons and use of them as children

--- advancedreact2/src/Components/Advanced/Advanced.jsx
+++ advancedreact2/src/Components/Advanced/Advanced.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button/Button";
+import {AiFillDollarCircle} from 'react-icons/ai'
 
 export default function Advanced (){
         return(
@@ -9,7 +10,7 @@ export default function Advanced (){
                 </p>
 
                 <h2>Button Component Props Review</h2>
-                <Button>Buy Now!</Button>
+                <Button><AiFillDollarCircle/>Buy Now!</Button>
             </div>
         )
     }
