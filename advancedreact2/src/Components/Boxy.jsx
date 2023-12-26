d635fe0ef96940facc7ceb2ddc1ce9dfcc17c8c3
Greg Rell <rell.greg@gmail.com>
2023-12-25 22:39:58 -0500

    end of render props

--- advancedreact2/src/Components/Boxy.jsx
+++ advancedreact2/src/Components/Boxy.jsx
@@ -7,8 +7,9 @@ export default function Boxy({children}){
         <Switcher>
             <Switcher.Button>
                 <Switcher.Display>
+                    {/* This is render props. switched is passed to the children in the Display component as children(switched) 
+                    this line below acts as a function, returning what its supposed to based on the state of switched*/}
                     {(switched)=>{return <div className={`box ${switched ? "blue":"red"}`}>{switched ? "blue":"red"}</div>}}
-           
                 </Switcher.Display>
             </Switcher.Button>            
         </Switcher>
