42e05520f2a34c41bd93f2af25d27ef0f57594ea
Greg Rell <rell.greg@gmail.com>
2023-12-31 12:29:38 -0500

    Beginning work on badge components with imports of Inter from google Fonts

--- advancedreact2/src/Components/Badge/Badge.jsx
+++ advancedreact2/src/Components/Badge/Badge.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import classnames from "classnames";
 
-export default function Badge({children, ...rest}){
+
+export default function Badge({children, className, color, variant, ...rest}){
+
+    const badgeColor = "gray"
+
+    let allClasses = classnames(className, badgeColor, variant, "badge")
+
+
+
     return (
         <>
+            <div className={allClasses}>{children && children}</div>
         </>
     )
 }
--- advancedreact2/src/Components/Routing/Library.jsx
+++ advancedreact2/src/Components/Routing/Library.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Badge from "../Badge/Badge";
+
 
 export default function Library(){
     return(
         <>
             <h2>Project Component Library</h2>
-            <a href='https://projects.scrimba.com/react-component-library' target="_blank">Scrimba Guidance</a>
+            <a href='https://projects.scrimba.com/react-component-library' target="_blank" rel="noopener noreferrer">Scrimba Guidance</a>
+            <br/>
+            <br/>
+            <br/>
+            <code>Badge Component</code>
+            <br/>
+            <Badge variant="square">Badge</Badge>
+
         </>
     )
 }
+
