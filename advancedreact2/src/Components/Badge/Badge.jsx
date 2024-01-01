bcdf10c2564f6109291474db89c41d25ccfc427b
Greg Rell <rell.greg@gmail.com>
2023-12-31 19:25:40 -0500

    Badge Components Implemented

--- advancedreact2/src/Components/Badge/Badge.jsx
+++ advancedreact2/src/Components/Badge/Badge.jsx
@@ -3,12 +3,8 @@ import classnames from "classnames";
 
 
 export default function Badge({children, className, color, variant, ...rest}){
-
-    const badgeColor = "gray"
-
-    let allClasses = classnames(className, badgeColor, variant, "badge")
-
-
+    //default if not provided : square and gray
+    let allClasses = classnames(className,  `${variant ? variant:"square"}`, `${color ? color:"gray"}`, "badge")
 
     return (
         <>
--- advancedreact2/src/Components/Routing/Library.jsx
+++ advancedreact2/src/Components/Routing/Library.jsx
@@ -8,11 +8,38 @@ export default function Library(){
             <h2>Project Component Library</h2>
             <a href='https://projects.scrimba.com/react-component-library' target="_blank" rel="noopener noreferrer">Scrimba Guidance</a>
             <br/>
-            <br/>
-            <br/>
-            <code>Badge Component</code>
-            <br/>
-            <Badge variant="square">Badge</Badge>
+           
+            <h3>Badge Component</h3>
+            <div className="presentation">
+                <div className="presentationPort">
+                    <ul>
+                        <li>Gray <Badge variant="square">Badge</Badge></li>
+                        <li>Red <Badge variant="square" color="red">Badge</Badge></li>
+                        <li>Yellow <Badge variant="square" color="yellow">Badge</Badge></li>
+                        <li>Green <Badge variant="square" color="green">Badge</Badge></li>
+                        <li>Blue <Badge variant="square" color="blue">Badge</Badge></li>
+                        <li>Indigo <Badge variant="square" color="indigo">Badge</Badge></li>
+                        <li>Purple <Badge variant="square" color="purple">Badge</Badge></li>
+                        <li>Pink <Badge variant="square" color="pink">Badge</Badge></li>
+                    </ul>
+             
+                </div>
+                <div className="presentationPort">
+                <ul>
+                        <li><Badge variant="pill">Badge</Badge></li>
+                        <li><Badge variant="pill" color="red">Badge</Badge></li>
+                        <li><Badge variant="pill" color="yellow">Badge</Badge></li>
+                        <li><Badge variant="pill" color="green">Badge</Badge></li>
+                        <li><Badge variant="pill" color="blue">Badge</Badge></li>
+                        <li><Badge variant="pill" color="indigo">Badge</Badge></li>
+                        <li><Badge variant="pill" color="purple">Badge</Badge></li>
+                        <li><Badge variant="pill" color="pink">Badge</Badge></li>
+                    </ul>
+                </div>
+            </div>
+
+            <h3>Banners</h3>
+         
 
         </>
     )
