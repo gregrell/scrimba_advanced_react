4b9db51637838f6f843c5c82aee1a3e1a5ff2380
Greg Rell <rell.greg@gmail.com>
2024-02-03 22:40:12 -0500

    comments

--- advancedreact2/src/Components/Routing/Performance.jsx
+++ advancedreact2/src/Components/Routing/Performance.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import Suspense from "../Suspense/Suspense";
 
-const ProductList = React.lazy(()=>{
-    return new Promise(resolve =>{
-      setTimeout(()=> resolve(import("../Suspense/ProductList")),3000)
-    })
-})
 
 
 export default function PerformancePage(){
 
     const[showProducts, setShowProducts] = React.useState(false)
 
+    const ProductList = React.lazy(()=>{
+        return new Promise(resolve =>{
+          setTimeout(()=> resolve(import("../Suspense/ProductList")),3000)
+        })
+    })
 
+
+    //Note: React.Suspense is not the same as Suspense component below. 
 
     return(
         <>
             <h2>Performance</h2>
             <h3>Lazy Loading and Suspense</h3>
             <button onClick={()=> setShowProducts(prev => !prev)}>Show Products</button>
-            <React.Suspense fallback={<Suspense/>}>
+            <React.Suspense fallback={<Suspense/>}> 
                 {showProducts && <ProductList/>}
             </React.Suspense>
 
